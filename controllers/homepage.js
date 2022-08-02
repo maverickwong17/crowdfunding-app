10ab92fba2a0ac7c2898ba38ac30a90b92c0d0a3
MKWong <107278431+maverickwong17@users.noreply.github.com>
2022-08-02 14:52:55 -0700

    preliminary homepage route

---
+++ controllers/homepage.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const { Project, User } = require('../../models');
+
+router.get('/', async(req,res) =>{
+    
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 const sess = {
   secret: 'Super secret secret',
